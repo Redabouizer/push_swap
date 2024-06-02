d3dd41d254a745b3cb7d94511678c461c0912b3c
Redabouizer <150621786+Redabouizer@users.noreply.github.com>
2024-06-02 22:01:58 +0100

    Add files via upload

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,23 +1,53 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "libft/libft.h"
 
-int main(int argc , char *argv[]){
-    char **list;
-    char *lit;
+int main(int argc, char *argv[]) {
+    if (argc < 2)
+        return (0);
+
     int i = 1;
-    while (i != argc)
-    {
-        int j = 0;
-        printf("%d : " , i);
-        while (argv[i][j]!='\0')
-        {
-            if (argv[i][j-1]==32) {
-                list = argv[i][j];
-                // list =ft_split(char const *s, " ");
-            }
-            j++;
-        }
-        i++;
-        printf("\n");
+    char **list = malloc(sizeof(char *) * argc);
+    if (!list) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return 1;
     }
+    int list_index = 0;
+
+    while (i < argc) {
+        char **tokens = ft_split(argv[i], ' ');
+
+        if (tokens) {
+            int j = 0;
+            while (tokens[j] != NULL) {
+                int id = 0;
+                int error = 0;
+                while (tokens[j][id] != '\0') {
+                    if ((tokens[j][id] < '0' || tokens[j][id] > '9') && tokens[j][id] != '-') {
+                        error = 1;
+                    }
+                    id++;
+                }
+                if (!error) {
+                    list[list_index++] = tokens[j];
+                } else {
+                    printf("ERREUR\n");
+                    free(tokens);
+                    free(list);
+                    return 0;
+                }
+                j++;
+            }
+            free(tokens);
+        }
+
+        i++;
+    }
+    printf("Tokens stored in list:\n");
+    for (int k = 0; k < list_index; k++) {
+        printf("%s\n", list[k]);
+    }
+    free(list);
+
+    return 0;
 }
