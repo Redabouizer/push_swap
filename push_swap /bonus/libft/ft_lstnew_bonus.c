1b9e26f0ac88be796fea04db40b490d0ca620437
Redabouizer <150621786+Redabouizer@users.noreply.github.com>
2024-07-19 01:06:21 +0100

    Add files via upload

---
+++ push_swap /bonus/libft/ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbouizer <rbouizer@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 17:56:37 by rbouizer          #+#    #+#             */
+/*   Updated: 2024/07/19 00:33:21 by rbouizer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_stack	*ft_lstnew(int index, int content)
+{
+	t_stack	*new_node;
+
+	new_node = (t_stack *)malloc(sizeof(t_stack));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->index = index;
+	new_node->next = NULL;
+	return (new_node);
+}
